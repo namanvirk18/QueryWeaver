87a34d23b03bc77e0a0252cf6c833d6b182006cc
Guy Korland <gkorland@gmail.com>
2025-08-26 11:28:25 +0300

    read valid once

--- api/auth/user_management.py
+++ api/auth/user_management.py
@@ -33,7 +33,7 @@ async def _get_user_info(api_token: str) -> Optional[Dict[str, Any]]:
             "api_token": api_token,
         })
 
-        if result.result_set and result.result_set[0][3]:
+        if result.result_set:
             token_valid = result.result_set[0][3]
 
             # TODO delete invalid token from DB
