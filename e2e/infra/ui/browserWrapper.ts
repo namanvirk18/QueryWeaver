4906b4cc17bd05876c714a5dd51c3a0ee75e1b78
Naseem Ali <34807727+Naseem77@users.noreply.github.com>
2025-12-10 12:21:32 +0200

    Add e2e Tests
    Initialize Playwright e2e test architecture

---
+++ e2e/infra/ui/browserWrapper.ts
@@ -0,0 +1,82 @@
+import { chromium, Browser, BrowserContext, Page, firefox } from 'playwright';
+import { test } from '@playwright/test';
+import BasePage from './basePage';
+
+async function launchBrowser(projectName: string): Promise<Browser> {
+    if (projectName.toLowerCase().includes('firefox')) {
+        return firefox.launch();
+    }
+
+    return chromium.launch();
+}
+
+export default class BrowserWrapper {
+
+    private browser: Browser | null = null;
+
+    private context: BrowserContext | null = null;
+
+    private page: Page | null = null;
+
+    async createNewPage<T extends BasePage>(PageClass: new (page: Page) => T, url?: string) {
+        if (!this.browser) {
+            const projectName = test.info().project.name;
+            this.browser = await launchBrowser(projectName);
+        }
+        if (!this.context) {
+            this.context = await this.browser.newContext();
+        }
+        if (!this.page) {
+            this.page = await this.context.newPage();
+        }
+        if (url) {
+            await this.navigateTo(url)
+        }
+
+        const pageInstance = new PageClass(this.page);
+        return pageInstance;
+    }    
+
+    getContext(): BrowserContext | null {
+        return this.context;
+    }
+
+    async getPage() {
+        if (!this.page) {
+            throw new Error('Browser is not launched yet!');
+        }
+        return this.page;
+    }
+
+    async setPageToFullScreen() {
+        if (!this.page) {
+            throw new Error('Browser is not launched yet!');
+        }
+        
+        await this.page.setViewportSize({ width: 1920, height: 1080 });
+    }
+
+    async navigateTo(url: string) {
+        if (!this.page) {
+            throw new Error('Browser is not launched yet!');
+        }
+        await this.page.goto(url);
+        await this.page.waitForLoadState('networkidle');
+    }
+
+    async closePage() {
+        if (this.page) {
+            await this.page.close();
+            this.page = null;
+        }
+    }
+
+    async closeBrowser() {
+        await this.closePage();
+        
+        if (this.browser) {
+            await this.browser.close();
+        }
+    }
+
+}
